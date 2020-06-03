4050cc39718a4288ee2e0a18860e526a6af68338
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-03 13:38:03 +0530

    Added: Heads Or Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,12 @@
-echo "Welcome"
+#!/bin/bash -x
+echo "Welcome to FlipCoin Combination Program"
+
+flip=$(( (($RANDOM % 2 )) +1 ));
+
+if [[ $flip -eq 1 ]];
+   then
+	echo "Heads"
+else
+        echo "Tails"
+fi
+
